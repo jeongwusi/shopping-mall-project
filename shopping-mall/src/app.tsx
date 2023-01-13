07c954cbf319c8a3624111648a52966b4ce833c8
jeongwusi <jeongwusi@gmail.com>
2023-01-13 21:42:30 +0900

    1월 13일

---
+++ shopping-mall/src/app.tsx
@@ -0,0 +1,18 @@
+import { QueryClientProvider } from "react-query";
+import { useRoutes } from "react-router-dom";
+import { routes } from "./routes";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { getClient } from "./queryClient";
+
+const App = () => {
+  const element = useRoutes(routes);
+  const queryClient = getClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      {element}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
