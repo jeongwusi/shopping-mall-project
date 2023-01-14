a868cefb3d0fab7c49375ef1f81611afb16ab5b3
jeongwusi <jeongwusi@gmail.com>
2023-01-14 10:53:32 +0900

    GNB

--- shopping-mall/src/app.tsx
+++ shopping-mall/src/app.tsx
@@ -3,12 +3,14 @@ import { useRoutes } from "react-router-dom";
 import { routes } from "./routes";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { getClient } from "./queryClient";
+import Gnb from "./components/gnb";
 
 const App = () => {
   const element = useRoutes(routes);
   const queryClient = getClient();
   return (
     <QueryClientProvider client={queryClient}>
+      <Gnb/>
       {element}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
