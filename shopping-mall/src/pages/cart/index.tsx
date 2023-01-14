4b3e02867bae379e5703cf3c83f1dd6a673e86b0
jeongwusi <jeongwusi@gmail.com>
2023-01-14 16:47:38 +0900

    장바구니 담기 API / 장바구니 페이지

--- shopping-mall/src/pages/cart/index.tsx
+++ shopping-mall/src/pages/cart/index.tsx
@@ -1,3 +1,16 @@
-const Cart = () => <div>장바구니</div>
+import { useQuery } from "react-query"
+import { CartType, GET_CART } from "../../graphql/cart"
+import { graphqlFetcher, QueryKeys } from "../../queryClient"
+import CartList from "../../components/cart"
+
+const Cart = () => {
+  const { data } = useQuery(QueryKeys.CART, () => graphqlFetcher(GET_CART))
+
+  const cartItems = Object.values(data || {}) as CartType[]
+
+  if(!cartItems.length) return <div>장바구니가 비어있어요!</div>
+
+  return <CartList items = {cartItems} />
+}
 
 export default Cart
