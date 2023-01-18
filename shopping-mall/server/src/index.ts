bd53e1bf4cde969c4d47a15dd8036d8fd395c4c5
jeongwusi <jeongwusi@gmail.com>
2023-01-18 23:45:08 +0900

    모노레포로 변경 / 서버 환경 설정

---
+++ shopping-mall/server/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+
+(async () => {
+  const server = new ApolloServer(null);
+
+  //   {
+  //   typeDefs: schema,
+  //   resolvers,
+  //   context: {
+
+  //   }
+
+  // })
+
+  const app = express();
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: "/graphql",
+    cors: {
+      origin: ["http://localhost:5173/"],
+      credentials: true,
+    },
+  });
+  await app.listen({ port: 8000 });
+  console.log("server listening on 8000...");
+})();
