d6b6c721d3c91cdc50bc68fb6cd431ea3ef04c5c
jeongwusi <jeongwusi@gmail.com>
2023-01-20 10:16:56 +0900

    무한스크롤 #2 - IntersectionObserver

--- shopping-mall/client/src/components/product/list.tsx
+++ shopping-mall/client/src/components/product/list.tsx
@@ -1,11 +1,13 @@
 import ProductItem from "./item";
 import { Product } from "../../graphql/products";
 
-const ProductList = ({ list }: { list: Product[] }) => (
+const ProductList = ({ list }: { list: { products: Product[] }[] }) => (
   <ul className="products">
-    {list.map((product) => (
-      <ProductItem {...product} key={product.id} />
-    ))}
+    {list.map((page) =>
+      page.products.map((product) => (
+        <ProductItem {...product} key={product.id} />
+      ))
+    )}
   </ul>
 );
 
--- shopping-mall/client/src/pages/products/index.tsx
+++ shopping-mall/client/src/pages/products/index.tsx
@@ -2,23 +2,45 @@ import { useInfiniteQuery } from "react-query";
 import GET_PRODUCTS, { Products } from "../../graphql/products";
 import { graphqlFetcher, QueryKeys } from "../../queryClient";
 import ProductList from "../../components/product/list";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const ProductListPage = () => {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery<Products>(
-    QueryKeys.PRODUCTS,
-    ({ pageParam = "" }) => graphqlFetcher(GET_PRODUCTS, { cursor: pageParam }),
-    {
-      getNextPageParam: (lastPage, allPages) => {
-        console.log(lastPage, allPages);
-        lastPage.products.at(-1)?.id;
-      },
+  const observerRef = useRef<IntersectionObserver>();
+  const fetchMoreRef = useRef<HTMLDivElement>(null);
+  const [intersecting, setIntersecting] = useState(false);
+
+  const getObserver = useCallback(() => {
+    if (!observerRef.current) {
+      observerRef.current = new IntersectionObserver((entries) => {
+        setIntersecting(entries[0]?.isIntersecting);
+      });
     }
-  );
+    return observerRef.current;
+  }, [observerRef.current]);
+
+  useEffect(() => {
+    if (fetchMoreRef.current) getObserver().observe(fetchMoreRef.current);
+  }, [fetchMoreRef.current]);
+
+  const { data, isFetchingNextPage, fetchNextPage, hasNextPage } =
+    useInfiniteQuery<Products>(
+      QueryKeys.PRODUCTS,
+      ({ pageParam = "" }) =>
+        graphqlFetcher(GET_PRODUCTS, { cursor: pageParam }),
+      {
+        getNextPageParam: (lastPage, allPages) => {
+          return lastPage.products.at(-1)?.id;
+        },
+      }
+    );
+
+    useEffect(() => {})
 
   return (
     <div>
       <h2>상품목록</h2>
-      <ProductList list={data?.products || []} />
+      <ProductList list={data?.pages || []} />
+      <div ref={fetchMoreRef} />
     </div>
   );
 };
